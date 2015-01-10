b90512718055dd194efccf8b1c028955abb4a653
Satori Canton <satoricanton@gmail.com>
2015-01-10 13:19:39 -0600

    Enabled routes and created views

--- app/coffee/script.coffee
+++ app/coffee/script.coffee
@@ -1,5 +1,50 @@
 socket = io()
-app = angular.module 'wolop-cms', []
+app = angular.module 'wolop-cms', ['ngRoute']
+
+app.config ($routeProvider) ->
+	path = $routeProvider.when
+
+	path '/',
+		templateUrl: '/partials/home.html'
+		controller: 'HomeController'
+
+	path '/websites',
+		templateUrl: '/partials/websites.html'
+		controller: 'WebsitesController'
+
+	path '/pages',
+		templateUrl: '/partials/pages.html'
+		controller: 'PagesController'
+
+	path '/menus',
+		templateUrl: '/partials/menus.html'
+		controller: 'MenusController'
+
+	path '/content-groups',
+		templateUrl: '/partials/content-groups.html'
+		controller: 'ContentGroupsController'
+
+	path '/users',
+		templateUrl: '/partials/users.html'
+		controller: 'UsersController'
+
+app.controller 'HomeController', ($scope) ->
+	console.log 'home-controller'
+
+app.controller 'WebsitesController', ($scope) ->
+	console.log 'websites-controller'
+
+app.controller 'PagesController', ($scope) ->
+	console.log 'pages-controller'
+
+app.controller 'MenusController', ($scope) ->
+	console.log 'menus-controller'
+
+app.controller 'ContentGroupsController', ($scope) ->
+	console.log 'content-groups-controller'
+
+app.controller 'UsersController', ($scope) ->
+	console.log 'users-controller'
 
 app.controller 'CmsController', ($scope) ->
 	$scope.isLoggedIn = false
@@ -19,3 +64,11 @@ app.directive 'userLogin', ->
 		$scope.loginData = {}
 		$scope.userLogin = (data) ->
 			socket.emit 'user-login', data
+
+app.directive 'createUser', ->
+	restrict: 'E'
+	templateUrl: '/partials/create-user.html'
+	controller: ($scope) ->
+		$scope.createUserData = {}
+		$scope.createUser = (data) ->
+			socket.emit 'create-user', data
--- app/public/javascripts/script.js
+++ app/public/javascripts/script.js
@@ -3,7 +3,60 @@
 
   socket = io();
 
-  app = angular.module('wolop-cms', []);
+  app = angular.module('wolop-cms', ['ngRoute']);
+
+  app.config(function($routeProvider) {
+    var path;
+    path = $routeProvider.when;
+    path('/', {
+      templateUrl: '/partials/home.html',
+      controller: 'HomeController'
+    });
+    path('/websites', {
+      templateUrl: '/partials/websites.html',
+      controller: 'WebsitesController'
+    });
+    path('/pages', {
+      templateUrl: '/partials/pages.html',
+      controller: 'PagesController'
+    });
+    path('/menus', {
+      templateUrl: '/partials/menus.html',
+      controller: 'MenusController'
+    });
+    path('/content-groups', {
+      templateUrl: '/partials/content-groups.html',
+      controller: 'ContentGroupsController'
+    });
+    return path('/users', {
+      templateUrl: '/partials/users.html',
+      controller: 'UsersController'
+    });
+  });
+
+  app.controller('HomeController', function($scope) {
+    return console.log('home-controller');
+  });
+
+  app.controller('WebsitesController', function($scope) {
+    return console.log('websites-controller');
+  });
+
+  app.controller('PagesController', function($scope) {
+    return console.log('pages-controller');
+  });
+
+  app.controller('MenusController', function($scope) {
+    return console.log('menus-controller');
+  });
+
+  app.controller('ContentGroupsController', function($scope) {
+    return console.log('content-groups-controller');
+  });
+
+  app.controller('UsersController', function($scope) {
+    return console.log('users-controller');
+  });
 
   app.controller('CmsController', function($scope) {
     $scope.isLoggedIn = false;
@@ -28,6 +81,19 @@
         $scope.loginData = {};
         return $scope.userLogin = function(data) {
           return socket.emit('user-login', data);
+        };
+      }
+    };
+  });
+
+  app.directive('createUser', function() {
+    return {
+      restrict: 'E',
+      templateUrl: '/partials/create-user.html',
+      controller: function($scope) {
+        $scope.createUserData = {};
+        return $scope.createUser = function(data) {
+          return socket.emit('create-user', data);
         };
       }
     };
